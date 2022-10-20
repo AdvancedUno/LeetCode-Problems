2d68fae2668144b91b52da0ab47cbd2b6e617d57
AdvancedUno <108073642+AdvancedUno@users.noreply.github.com>
2022-10-20 11:32:26 +0900

    Time: 61 ms (5.19%), Space: 11.6 MB (13.48%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,81 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        
+        vector<pair<int, string>> storeDeno;
+        storeDeno.push_back(make_pair(1000, "M"));
+        storeDeno.push_back(make_pair(500, "D"));
+        storeDeno.push_back(make_pair(100, "C"));
+        storeDeno.push_back(make_pair(50, "L"));
+        storeDeno.push_back(make_pair(10, "X"));
+        storeDeno.push_back(make_pair(5, "V"));          
+        storeDeno.push_back(make_pair(1, "I"));                    
+        
+        vector<pair<int, string>> storeSpecialCase;
+        storeSpecialCase.push_back(make_pair(100, "C"));
+        storeSpecialCase.push_back(make_pair(10, "X"));
+        storeSpecialCase.push_back(make_pair(1, "I"));
+
+        
+        int divNum = 0, leftOverNum = num, checkNum = 0;
+        
+        string outputString = "";
+                      
+        for(int i = 0; i < storeDeno.size(); i ++){
+            cout << leftOverNum << endl;
+            if(leftOverNum == 0)break;
+            bool checkSpecial = false;
+            divNum = leftOverNum/storeDeno[i].first;
+            checkNum = leftOverNum;
+            
+            leftOverNum = num % storeDeno[i].first;
+            
+            //cout << divNum << endl;
+            //cout << checkNum << endl;
+            if(divNum == 0)continue;
+            
+            for(int k = 0; k < storeSpecialCase.size(); k ++){
+                
+                if(checkNum/storeSpecialCase[k].first == 9){
+                    outputString += storeSpecialCase[k].second;
+                    
+                    outputString += storeDeno[i-1].second;
+                    
+                    leftOverNum = checkNum - storeSpecialCase[k].first * 9;
+                    checkSpecial = true;
+                    i++;
+                    //cout<<leftOverNum << endl;
+                    break;
+                }
+                else if(checkNum/storeSpecialCase[k].first == 4){
+                    outputString += storeSpecialCase[k].second;
+                    outputString += storeDeno[i-1].second;
+                    
+                    //outputString += storeSpecialCase[k].second;
+                    leftOverNum = checkNum - storeSpecialCase[k].first * 4;
+                    checkSpecial = true;
+                    
+                    //cout<<leftOverNum << endl;
+                    break;
+                }
+            }
+            if(checkSpecial == true)continue;
+            
+            
+            for(int j = 0; j < divNum; j ++){
+                outputString += storeDeno[i].second;
+            }
+            
+        }
+
+        
+        
+        
+        
+        return outputString;
+        
+        
+        
+        
+    }
+};
