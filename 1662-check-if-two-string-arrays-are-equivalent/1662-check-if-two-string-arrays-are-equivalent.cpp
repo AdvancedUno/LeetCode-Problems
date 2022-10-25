5e4ee280d909b6aac5d2992a74f953465d9ce341
AdvancedUno <108073642+AdvancedUno@users.noreply.github.com>
2022-10-25 09:48:53 +0900

    Time: 3 ms (93.69%), Space: 11.6 MB (27.47%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -2,30 +2,23 @@ class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
         string tempWord1 = "";
-        string tempWord2 = "";
+        int cnt2 = 0;
         for(string wString : word1){
             tempWord1 += wString;
         }
         
-        for(string wString : word2){
-            tempWord2 += wString;
+        int i;
+        for(i = 0; i < tempWord1.length();){
+            for(string wString : word2){
+                for(int j = 0; j < wString.length(); j ++){
+                    if(tempWord1[i] != wString[j])return false;
+                    i++;
+                    cnt2++;
+            }
+            }
         }
-        
-        if(tempWord1.length() != tempWord2.length())return false;
-        
-        for(int i = 0; i <tempWord1.length(); i ++){
-            if(tempWord1[i] != tempWord2[i])return false;            
-        }
-        
-        // for(int i = 0; i < tempWord1.length(); i ++){
-        //     for(string wString : word2){
-        //         for(int j = 0; j < wString.length(); j ++){
-        //             if(tempWord1[i] != wString[j])return false;
-        //             i++;
-        //     }
-        //     }
-        // }
-        
+        cout << cnt2 << " " << i << endl;
+        if(i != cnt2)return false;
         
         
         
