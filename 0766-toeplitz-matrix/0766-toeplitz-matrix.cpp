c538f652286b32d0281c71e0c1215ca83a5f7aa1
EunHo Lee <108073642+AdvancedUno@users.noreply.github.com>
2022-10-31 11:01:59 +0900

    Time: 22 ms (78.49%), Space: 17.5 MB (15.16%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -3,25 +3,17 @@ public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
         
         for(int i = 0; i < matrix.size(); i ++){
-            
             for(int j = 0; j < matrix[i].size(); j ++){
-                int x = j+1;
-                int y = i+1;
-                while(y < matrix.size() && x < matrix[i].size()){
-                    if(matrix[i][j] != matrix[y][x]){
+                int x = 1;
+
+                while(i+x < matrix.size() && j+x < matrix[i].size()){
+                    if(matrix[i][j] != matrix[i+x][j+x]){
                         return false;
                     }
-                    y++;
                     x++;
                 }
             }
-            
-            
-            
-            
         }
-        
-        
         return true;
         
     }
