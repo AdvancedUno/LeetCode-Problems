566c2896c7e6e8cf6c7118e6e9825b8fcaccee0a
EunHo Lee <108073642+AdvancedUno@users.noreply.github.com>
2022-11-07 10:58:12 +0900

    Time: 0 ms (100.00%), Space: 6.4 MB (85.14%) - LeetHub

---
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+    public:
+    string orderlyQueue(string S, int k)
+    {
+        if (k > 1){
+            sort(S.begin(), S.end());
+            return S;
+        } 
+
+
+        if (k == 1)
+        {
+            deque<char> s(begin(S), end(S));
+            auto mn = s;
+            for (auto i = 1; i < S.size(); ++i)
+                s.push_back(s[0]), s.pop_front(), mn = min(mn, s);
+            return string(begin(mn), end(mn));
+        }
+        return S;
+    }
+};
