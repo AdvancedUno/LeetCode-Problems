0fe549b0eb64aa62f60fcc34722e3a550621ea16
EunHo Lee <108073642+AdvancedUno@users.noreply.github.com>
2022-11-14 09:23:14 +0900

    Time: 18 ms (34.44%), Space: 7.5 MB (67.61%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -10,7 +10,6 @@ public:
     
     if(s[s.length()-1] == ' '){
         lastInclude = false;
-        cout << "false" << endl;
     }
 	for(int i = 0; i < s.length(); i ++){
 
@@ -20,23 +19,17 @@ public:
         }
 	
 		if(s[i] == ' ' || i == s.length()-1){
-            if(i == s.length()-1){
-                cout << s[i] << endl;
-                cout << s.substr(start, i - start) << endl;
-                cout << start << endl;
-                cout << i << endl;
-            }
+           
             if(i == s.length()-1 && lastInclude){
                 i++;
-                cout << newWord << endl;
-                //cout << "yes" << endl;
+
             } else if(i == s.length()-1 && !lastInclude)break;
             
 
             
 			if(newWord == false){
 				string subS = s.substr(start, i - start);
-                cout << subS << endl;
+                
 				saveS.push(subS);
 			}
 			newWord = true;
@@ -56,13 +49,12 @@ public:
     while(saveS.size() > 1){
         resultS += saveS.top();
         resultS += " ";
-        cout << saveS.top() << " ";
+
         saveS.pop();
         
     }
     resultS += saveS.top();
-    cout << saveS.top();
-        
+
     saveS.pop();
     
 	
