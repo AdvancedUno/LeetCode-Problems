b07c935d4c755ce6d86958153ccc6eaf7b6111cf
EunHo Lee <108073642+AdvancedUno@users.noreply.github.com>
2022-11-14 09:38:55 +0900

    Time: 7 ms (81.69%), Space: 8.1 MB (45.11%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,64 +1,17 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        
+        vector<string> words;
+        stringstream ss(s);
+        string tmp;
+        while (ss >> tmp)
+            words.push_back(tmp);
 
-	int start = 0;
-	bool newWord = true;
-	stack<string> saveS;
-    bool lastInclude = true;
-    
-   	 if(s[s.length()-1] == ' '){
-        	lastInclude = false;
-         
-   	 }
-        
-	for(int i = 0; i < s.length(); i ++){
-
-        if(i == s.length() -1 ){
-            if(lastInclude == false)break;
-            if(newWord == true)start = i;
-            
-            newWord = false;
-            //cout << saveS.top() << endl;
-            i++;
+        string ans;
+        for (int i = words.size() - 1; i >= 0; --i) {
+            if (i != words.size() - 1) ans += " ";
+            ans += words[i];
         }
-	
-	    if(s[i] == ' ' || i == s.length()){
-           
-            if(newWord == false){
-                string subS = s.substr(start, i - start);
-                //cout << subS << endl;
-                saveS.push(subS);
-            }
-            newWord = true;
-
-
-        }else{
-            if(newWord == true){
-                start = i;
-                newWord = false;
-            }
-        }
-		
-	}
-        
-    string resultS = "";
-        
-    while(saveS.size() > 1){
-        resultS += saveS.top();
-        resultS += " ";
-
-        saveS.pop();
-        
+        return ans;
     }
-    resultS += saveS.top();
-
-    saveS.pop();
-    
-	
-    return resultS;
-
-    }
-
 };
