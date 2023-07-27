9aaf58bee7640045c01bf7b8f75ca1299a716d34
EunHo Lee <108073642+AdvancedUno@users.noreply.github.com>
2023-07-27 19:25:27 +0900

    Time: 187 ms (59.71%), Space: 55.7 MB (98.56%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     long long maxRunTime(int n, vector<int>& b) {
-        long long low =1;
-        long long high = accumulate(begin(b),end(b),0LL)/n;
-
-        while(low < high){
-            long long mid = (high+low+1)/2;
-            long long time=0;
-            for(int t : b) time += min((long long)t,mid);
-
-            if(mid*n <= time) low = mid;
-            else high = mid-1;
+        int m = b.size();
+        sort(b.begin(),b.end());
+        long long sum = accumulate(begin(b),end(b),0LL);
+        int i=m-1;
+        while(i>=0 && sum/n < b[i] ){
+            sum -= b[i];
+            n--;i--;
         }
-        return low;
+        return sum/n;
     }
 };
