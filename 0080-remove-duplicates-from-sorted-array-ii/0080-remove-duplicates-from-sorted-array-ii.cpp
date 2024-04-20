61b4af3de344b8b1a94ac3f2834017b08752e02b
EunHo Lee <108073642+AdvancedUno@users.noreply.github.com>
2024-04-20 00:01:06 -0400

    Time: 3 ms (74.60%), Space: 14 MB (8.18%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,11 +1,30 @@
 class Solution {
 public:
 
-  int removeDuplicates(vector<int>& nums) {
-    int i = 0;
-    for (int n : nums)
-        if (i < 2 || n > nums[i-2])
-            nums[i++] = n;
-    return i;
-}
+    int removeDuplicates(vector<int>& nums) {
+
+        map<int , int> store ;
+
+        for(int i = 0; i < nums.size(); i ++){
+            if(store.find(nums[i]) != store.end()){
+                store[nums[i]] +=1;
+            }else{
+                store[nums[i]] = 1;
+            }
+        }
+        
+        int total_size = 0;
+
+        for (auto s = store.begin(); s != store.end(); s++) {
+            for(int i = 0; i < min(2, s->second); i ++){
+                nums[total_size] = s->first;
+                total_size ++;
+            }
+        }
+        
+            
+      return total_size;
+      
+      
+    }
 };
